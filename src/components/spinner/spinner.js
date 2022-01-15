3640f69fc30f202106f6abe158d60da443c171c6
Andrii <www.andriy12@gmail.com>
2022-01-15 22:05:23 +0200

    working with API ice and fire

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './spinner.css';
+
+
+const Spinner = () => {
+    return(
+        <div class="loadingio-spinner-spin-x4zz31wd2xb"><div class="ldio-ah97d9dvib">
+        <div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div>
+        </div></div>
+    )
+}
+
+
+export default Spinner;
+
+
+
